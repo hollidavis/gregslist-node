22eae90e5b813a31ecfbcaf73a12834eed985ae7
hollidavis <hollidavis19@gmail.com>
2021-07-27 15:18:54 -0600

    job model created

--- server/models/Job.js
+++ server/models/Job.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema
 
 const Job = new Schema(
   {
-
+    jobTitle: { type: String, required: true },
+    rate: { type: Number, required: true },
+    hours: { type: Number, required: true },
+    company: { type: String, required: true },
+    description: { type: String }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
